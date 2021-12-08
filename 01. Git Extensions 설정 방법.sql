d439d135b1e8f6de6044805fc2e2e008a1e84fda
JINTAEMAN <tamario@naver.com>
2021-12-08 14:33:06 +0900

    [DEV] [SI-메뉴얼게시판] [공통] [진태만] Git Extensions 설정 방법

---
+++ 01. Git Extensions 설정 방법.sql
@@ -0,0 +1,49 @@
+
+
+- Git Extensions 설정 방법 @@@
+01. 저장소 생성(웹(https://github.com)에서)
+https://github.com/JINTAEMAN/01_Git_TAMA   ----> INTAEMAN/22tadhtlqenf!
+2) HTTPS: https://github.com/JINTAEMAN/01_Git_TAMA.git
+----------------------------------------------------------------------------------------------------
+
+02. 저장소 Clone(Clone repository)  @@@
+1. 로컬 탐색기에서 D:\91_Git_TAMA 폴더: 마우스 우 클릭 ==> GitExt Clone
+2. Clone(새창)
+ 1) Repositories to clone: https://github.com/JINTAEMAN/01_Git_TAMA.git
+ 2) Destination: D:\91_Git_TAMA
+ 3) Subdirectory to create:  01_TEST
+ 4) Branch:  (default: remote HEAD) 
+ 5) repository type: Personal repository --> Clone: 클릭
+==> .git 생성됨(D:\91_Git_TAMA\01. TEST)	 ===> ■■■
+3. D:\91_Git_TAMA\01_TEST 폴더: 마우스 우 클릭 ==> GitExt Open repository
+ 
+03. 저장소 수정 테스트 @@@
+1. D:\91_Git_TAMA\01. TEST 폴더에서 T_01.sql 파일 생성
+2. Git Extensions 화면에서 Tab(Commit(1))로 표시됨  ==> Commit(1)): 클릭
+3. Commit 화면(새창)
+ 1)  T_01.sql 파일 ==> Stage 버튼 클릭
+ 2) Comment messsage: [DEV] [SI-메뉴얼게시판] [공통] [진태만] AS 게시판 관련 수정
+ 3) Commit: 클릭
+ 4) Process(새창) 
+  Done   ==> OK: 클릭
+4. ▶ main [DEV] [SI-메뉴얼게시판] [공통] [진태만] AS 게시판 관련 수정  ===> 표시됨 ■■■ 
+----------------------------------------------------------------------------------------------------
+
+5. Git Extensions 화면에서 Tab(▲) : 클릭
+ 1) Push(새창) 
+   가. Remote: origin
+   나. Branch to push: main to amin    ==> Push: 클릭
+   다. Process(새창) 
+    Done  ==> OK: 클릭 
+ 2) Push 확인
+   가. https://github.com/JINTAEMAN/01_TEST ==> JINTAEMAN/01_TEST  
+JINTAEMAN [DEV] [SI-메뉴얼게시판] [공통] [진태만] Git Extensions 설정 방법_02	f0f04ba		1 minute ago	  4 commits
+T_01.sql				[DEV] [SI-메뉴얼게시판] [공통] [진태만] AS 게시판 관련 수정			5 minutes ago
+T_02_remote.txt	T_02_remote.txt					2 hours ago
+T_03.sql				[DEV] [SI-메뉴얼게시판] [공통] [진태만] Git Extensions 설정 방법_02 	2 minutes ago	===> 표시됨 ■■■ 
+==================================================================================================================
+■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■ 
++----------------------------------------------------// End //----------------------------------------------------+
+■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
+
+ 
